8c9b7fe45c5ac4a16600127787d0b08db081f462
Alejandro Aristizabal <alejandroiquira3@gmail.com>
2022-12-26 19:36:33 -0500

    Activate order method and flexipage

--- force-app/main/default/lwc/orderProductsLwc/orderProductsLwc.js
+++ force-app/main/default/lwc/orderProductsLwc/orderProductsLwc.js
@@ -1,6 +1,8 @@
 import { LightningElement,wire,track,api} from 'lwc';  
   import {refreshApex} from '@salesforce/apex';  
   import getOrderProducts from '@salesforce/apex/ProductUtility.getOrderProductsList';  
+  import activateOrder from '@salesforce/apex/ProductUtility.activateOrder';  
+  import { ShowToastEvent } from 'lightning/platformShowToastEvent'
   const COLS=[  
   {label:'Name',fieldName:'Product2.Name', type:'text'},   
   {label:'Quantity',fieldName:'Quantity', type:'currency'}  ,   
@@ -20,45 +22,36 @@ import { LightningElement,wire,track,api} from 'lwc';
     }
     
   };  
-
-
-  
-  renderedCallback() {
-    console.log ("renderedCallback Record Id:"+ this.recordId);
-    getOrderProducts ({orderid:this.recordId}).then (result =>
+    
+    activateOrder(){
+        
+      console.log("ActivateOrder1");
+      activateOrder({orderid : this.recordId}).then(result=>
         {
-            console.log ("Result UnitPrice:"+result[0].UnitPrice);
-            let items=[];
-            var orderProductsList = result;
-
-            console.log ("orderProductsList.length:"+orderProductsList.length);
-            /*
-            for (let i=0; i< orderProductsList.length; i++ )
-            {
-                var item ={
-                    
-                label:'ID',fieldName:'Id.Name', type:'text', value:   orderProductsList[i].Id,
-                label:'Name',fieldName:'Product2.Name', type:'text', value:   orderProductsList[i].Product2,
-                label:'Quantity',fieldName:'Quantity', type:'currency', value:   orderProductsList[i].Quantity,   
-                label:'Unit Price',fieldName:'UnitPrice', type:'currency', value:   orderProductsList[i].UnitPrice,
-                label:'Total Price',fieldName:'TotalPrice', type:'currency', value:   orderProductsList[i].TotalPrice    
-                }
-                items.push(item);
-                console.log("Item"+ orderProductsList[i].Id);
-            }
-            console.dir (items);
-            this.listaproductos= items;
-            console.dir (result);
-            */
-            //this.products=result;
-            }
-        );    
+          console.log("ActivateOrder2");
+          this.showToast('Success', result, 'Success', 'dismissable');
+          //eval("$A.get('e.force:refreshView').fire();");
+          this.updateRecordView();
+        }
+      ).catch(error=>{  
+        alert('Error activating order'+JSON.stringify(error));  
+      })
     }
 
-    
-    activateOrder(evt){
-        
-        
-    };
+    showToast(title, message, variant, mode) {
+      const event = new ShowToastEvent({
+          title: title,
+          message: message,
+          variant: variant,
+          mode: mode
+      });
+      this.dispatchEvent(event);
+    }
   
- }  
+    updateRecordView() {
+      setTimeout(() => {
+           eval("$A.get('e.force:refreshView').fire();");
+      }, 1000); 
+    }
+
+ }
